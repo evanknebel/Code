42eacc3f8c3cf2b46d7aa58d88658398596c7374
s179060 <s179060@SEA-8D0A81>
2017-08-14 12:58:06 -0700

    Add project files.

---
+++ 1-HelloAIE/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello AIE" << std::endl;
+	system("pause");
+
+	std::cout << "" << std::endl;
+	std::cout << "Some good songs: " << std::endl;
+	std::cout << "Virtual Breakup by Phoenix #2772" << std::endl;
+	std::cout << "Roy's Scat by J.A.M" << std::endl;
+	std::cout << "Snake Eater from MGS3" << std::endl;
+	std::cout << "and others, too." << std::endl;
+	system("pause");
+
+	std::cout << "" << std::endl;
+	std::cout << "Now for some numbers:" << std::endl;
+	std::cout << "0" << std::endl;
+	std::cout << "1" << std::endl;
+	std::cout << "2" << std::endl;
+	std::cout << "3" << std::endl;
+	std::cout << "4" << std::endl;
+	std::cout << "5" << std::endl;
+	system("pause");
+
+	std::cout << "" << std::endl;
+	std::cout << "And back down." << std::endl;
+	std::cout << "5" << std::endl;
+	std::cout << "4" << std::endl;
+	std::cout << "3" << std::endl;
+	std::cout << "2" << std::endl;
+	std::cout << "1" << std::endl;
+	std::cout << "0" << std::endl;
+	std::cout << "Great job." << std::endl;
+	system("pause");
+}
