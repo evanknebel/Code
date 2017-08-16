2ea3fee52fbd5b0db4c6e6dbf2dd6c5ec3bd6c09
s179060 <s179060@students.aue.edu.au>
2017-08-16 10:12:35 -0700

    made progress

--- 4-Functions/functionsMain.cpp
+++ 4-Functions/functionsMain.cpp
@@ -17,11 +17,31 @@ double decimal(double a, double b)
 	return a / b;
 }
 
+int clamp(double a)
+{
+	std::cout << "" << std::endl;
+	int min = 15;
+	int max = 30;
+	if (a < min)
+	{
+		return min;
+	}
+	else if (a > max)
+	{
+		return max;
+	}
+	else
+	{
+		return a;
+	}
+}
+
 int main()
 {
 	hello();
 	std::cout << square(5);
 	std::cout << decimal(2 , 5);
+	std::cout << clamp(16);
 
 	system("pause");
 
