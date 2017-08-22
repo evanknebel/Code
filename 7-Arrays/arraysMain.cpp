9f0e18d990b72ad93b06b1a4a85507a6ea01001a
s179060 <s179060@students.aie.edu.au>
2017-08-22 10:26:25 -0700

    work on arrays

--- 7-Arrays/arraysMain.cpp
+++ 7-Arrays/arraysMain.cpp
@@ -8,21 +8,48 @@ void printNumbers(int nums[], int size)
 	}
 }
 
+int sumNumbers(int num[], int size)
+{
+	int total = 0;
+	for (int i = 0; i < size; i++)
+	{
+		total = total + num[i];
+	}
+	std::cout << total << std::endl;
+	return total;
+}
 
+void reverse(int num[], int size)
+{
+	for (int i = 0; i < size / 2; i++)
+	{
+		int temp = num[i];
+		//front to back
+		num[i] = num[size - 1 - i];
+		//back to front
+		num[size - 1 - i] = temp;
+	}
+}
+
+void sortAscend(int num[], int size)
+{
+	while (true)
+	{
+		bool isSorted = true;
+		for (int i = 0; )
+	}
+}
 
 int main()
 {
-	int values[]{1, 3, 5, 7, 9};
+	int values[]{3, 5, 1, 2, 4};
 
 	printNumbers(values, 5);
+	sumNumbers(values, 5);
+	reverse(values, 5);
+	printNumbers(values, 5);
+	reverse(values, 5);
+
 
 	system("pause");
 }
-
-
-
-
-
-
-
-
