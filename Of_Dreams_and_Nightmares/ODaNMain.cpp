615536b684a3cc022ab1d3cf5e6dff9a212b1b7f
evanknebel <s179060@students.aie.edu.au>
2017-08-28 15:15:40 -0700

    Began ODaN

---
+++ Of_Dreams_and_Nightmares/ODaNMain.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+float speed = 30;
+
+/*
+Parry:
+Base 25% chance. 1/2 speed added to percentage. Enters 100% counterattack. 15% damage
+added to counterattack.
+Failing results in full damage. Enters counterattack. No attack bonus.
+
+Block:
+Base 100% chance. Recieve 20% damage. Enters counterattack.
+
+Dodge:
+Base 45% chance. 1/2 speed added to percentage.
+Failing results in full damage. Enters counterattack.
+
+Attack:
+Base 100% chance. Recieve full damage. Enters 100% counterattack.
+
+Counterattack:
+Base 60% chance. 1/2 speed added to percentage.
+*/
+
+int main
+{
+
+}
