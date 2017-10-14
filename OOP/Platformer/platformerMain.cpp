d39ddc994f7f1814b75df81619bb719dc609a264
evanknebel <lupeling@gmail.com>
2017-10-14 16:47:50 -0700

    Changed values for items and platforms to use structs and classes, respectively

--- OOP/Platformer/platformerMain.cpp
+++ OOP/Platformer/platformerMain.cpp
@@ -12,17 +12,17 @@ int WINDOW_HEIGHT = 1000; //1400
 //movement speed (lateral)
 int DEFAULT_SPEED = 9;
 int P1SPEED = DEFAULT_SPEED;
-int P2SPEED = 3;
+//int P2SPEED = 3;
 
 //p1 character properties
 int P1RADIUS = 10;
 int P1X = 0;
 int P1Y = 0;
 
-//p2 character properties
-int P2X = 700;
-int P2Y = 235;
-int P2RADIUS = 70;
+////p2 character properties
+//int P2X = 700;
+//int P2Y = 235;
+//int P2RADIUS = 70;
 
 //jump values
 int JUMP_TIMER = 0;
@@ -30,6 +30,9 @@ bool isJumping = false;
 int JUMP_SPEED = 10;
 int JUMP_HEIGHT = 450;
 
+
+
+/*
 //platform 1 properties
 int PLATFORM_1_HEIGHT = 300;
 int PLATFORM_1_WIDTH = 400;
@@ -42,7 +45,8 @@ int PLATFORM_2_BASE_POSITION = 600;
 int PLATFORM_3_HEIGHT = 850;
 int PLATFORM_3_WIDTH = 60;
 int PLATFORM_3_BASE_POSITION = P1RADIUS * 2 + 1;
-
+*/
+/*
 //item properties
 int ITEM_DEFAULT_RADIUS = 4;
 //item 1 properties
@@ -60,6 +64,7 @@ int ITEM_3_RADIUS = ITEM_DEFAULT_RADIUS;
 int ITEM_3_X = PLATFORM_3_BASE_POSITION + (PLATFORM_3_WIDTH / 2);
 int ITEM_3_Y = PLATFORM_3_HEIGHT + P1RADIUS;
 bool isItem3Collected = false;
+*/
 
 void detectCollisionBoundries()
 {
@@ -96,8 +101,56 @@ void canJump()
 		}
 	}
 }
+
+class Platform
+{
+public:
+	int basePosition;
+	int width;
+	int height;
+};
+Platform platform1, platform2, platform3;
+
+struct Item
+{
+	int radius;
+	int X;
+	int Y;
+	bool isCollected;
+};
+Item item1, item2, item3;
+
 int main()
 {
+	//platform 1
+	platform1.basePosition = 80;
+	platform1.width = 400;
+	platform1.height = 300;
+	//platform 2
+	platform2.basePosition = 600;
+	platform2.width = 250;
+	platform2.height = 600;
+	//platform 3
+	platform3.basePosition = P1RADIUS * 2 + 1;
+	platform3.width = 60;
+	platform3.height = 850;
+
+	//item 1
+	item1.radius = 4;
+	item1.X = platform1.basePosition + (platform1.width / 2);
+	item1.Y = platform1.height + P1RADIUS;
+	item1.isCollected = false;
+	//item 2
+	item2.radius = 4;
+	item2.X = platform2.basePosition + (platform2.width / 2);
+	item2.Y = platform2.height + P1RADIUS;
+	item2.isCollected = false;
+	//item 3
+	item3.radius = 4;
+	item3.X = platform3.basePosition + (platform3.width / 2);
+	item3.Y = platform3.height + P1RADIUS;
+	item3.isCollected = false;
+
 	//create draw window
 	sfw::initContext(WINDOW_WIDTH, WINDOW_HEIGHT, "SFW");
 
@@ -110,50 +163,50 @@ int main()
 	while (sfw::stepContext())
 	{
 		//build platforms
-		sfw::drawLine(PLATFORM_1_BASE_POSITION, PLATFORM_1_HEIGHT, PLATFORM_1_BASE_POSITION + PLATFORM_1_WIDTH, PLATFORM_1_HEIGHT);
-		sfw::drawLine(PLATFORM_2_BASE_POSITION, PLATFORM_2_HEIGHT, PLATFORM_2_BASE_POSITION + PLATFORM_2_WIDTH, PLATFORM_2_HEIGHT);
-		sfw::drawLine(PLATFORM_3_BASE_POSITION, PLATFORM_3_HEIGHT, PLATFORM_3_BASE_POSITION + PLATFORM_3_WIDTH, PLATFORM_3_HEIGHT);
+		sfw::drawLine(platform1.basePosition, platform1.height, platform1.basePosition + platform1.width, platform1.height);
+		sfw::drawLine(platform2.basePosition, platform2.height, platform2.basePosition + platform2.width, platform2.height);
+		sfw::drawLine(platform3.basePosition, platform3.height, platform3.basePosition + platform3.width, platform3.height);
 
 		//items
 		//draw item 1 if not collected, and stop drawing when collected
-		if (isItem1Collected == false)
+		if (item1.isCollected == false)
 		{
-			sfw::drawCircle(ITEM_1_X, ITEM_1_Y, ITEM_1_RADIUS);
+			sfw::drawCircle(item1.X, item1.Y, item1.radius);
 		}
-		if (ITEM_1_X >= P1X - P1RADIUS && ITEM_1_X <= P1X + P1RADIUS)
+		if (item1.X >= P1X - P1RADIUS && item1.X <= P1X + P1RADIUS)
 		{
-			if (ITEM_1_Y >= P1Y - P1RADIUS && ITEM_1_Y <= P1Y + P1RADIUS)
+			if (item1.Y >= P1Y - P1RADIUS && item1.Y <= P1Y + P1RADIUS)
 			{
-				isItem1Collected = true;
+				item1.isCollected = true;
 			}
 		}
 		//draw item 2 if not collected, and stop drawing when collected
-		if (isItem2Collected == false)
+		if (item2.isCollected == false)
 		{
-			sfw::drawCircle(ITEM_2_X, ITEM_2_Y, ITEM_2_RADIUS);
+			sfw::drawCircle(item2.X, item2.Y, item2.radius);
 		}
-		if (ITEM_2_X >= P1X - P1RADIUS && ITEM_2_X <= P1X + P1RADIUS)
+		if (item2.X >= P1X - P1RADIUS && item2.X <= P1X + P1RADIUS)
 		{
-			if (ITEM_2_Y >= P1Y - P1RADIUS && ITEM_2_Y <= P1Y + P1RADIUS)
+			if (item2.Y >= P1Y - P1RADIUS && item2.Y <= P1Y + P1RADIUS)
 			{
-				isItem2Collected = true;
+				item2.isCollected = true;
 			}
 		}
 		//draw item 3 if not collected, and stop drawing when collected
-		if (isItem3Collected == false)
+		if (item3.isCollected == false)
 		{
-			sfw::drawCircle(ITEM_3_X, ITEM_3_Y, ITEM_3_RADIUS);
+			sfw::drawCircle(item3.X, item3.Y, item3.radius);
 		}
-		if (ITEM_3_X >= P1X - P1RADIUS && ITEM_3_X <= P1X + P1RADIUS)
+		if (item3.X >= P1X - P1RADIUS && item3.X <= P1X + P1RADIUS)
 		{
-			if (ITEM_3_Y >= P1Y - P1RADIUS && ITEM_3_Y <= P1Y + P1RADIUS)
+			if (item3.Y >= P1Y - P1RADIUS && item3.Y <= P1Y + P1RADIUS)
 			{
-				isItem3Collected = true;
+				item3.isCollected = true;
 			}
 		}
 
 		//debug
-		std::cout << JUMP_TIMER << "           " << P1X << "           " << P1Y << "           " << isItem1Collected << "           " << isItem2Collected << "           " << isItem3Collected << std::endl;
+		std::cout << JUMP_TIMER << "           " << P1X << "           " << P1Y << "           " << item1.isCollected << "           " << item1.isCollected << "           " << item3.isCollected << std::endl;
 
 
 		////////player 1 stuff
@@ -220,37 +273,37 @@ int main()
 		}
 		
 		//platform 1
-		if (P1Y - P1RADIUS >= PLATFORM_1_HEIGHT - JUMP_SPEED && P1Y + P1RADIUS <= PLATFORM_1_HEIGHT + P1RADIUS * 2)
+		if (P1Y - P1RADIUS >= platform1.height - JUMP_SPEED && P1Y + P1RADIUS <= platform1.height + P1RADIUS * 2)
 		{
-			if (P1X - P1RADIUS < PLATFORM_1_BASE_POSITION + PLATFORM_1_WIDTH && P1X + P1RADIUS > PLATFORM_1_BASE_POSITION)
+			if (P1X - P1RADIUS < platform1.basePosition + platform1.width && P1X + P1RADIUS > platform1.basePosition)
 			{
-				P1Y = PLATFORM_1_HEIGHT + P1RADIUS;
+				P1Y = platform1.height + P1RADIUS;
 				canJump();
 			}
 		}
 
 		//platform 2
-		if (P1Y - P1RADIUS >= PLATFORM_2_HEIGHT - JUMP_SPEED && P1Y + P1RADIUS <= PLATFORM_2_HEIGHT + P1RADIUS * 2)
+		if (P1Y - P1RADIUS >= platform2.height - JUMP_SPEED && P1Y + P1RADIUS <= platform2.height + P1RADIUS * 2)
 		{
-			if (P1X - P1RADIUS < PLATFORM_2_BASE_POSITION + PLATFORM_2_WIDTH && P1X + P1RADIUS > PLATFORM_2_BASE_POSITION)
+			if (P1X - P1RADIUS < platform2.basePosition + platform2.width && P1X + P1RADIUS > platform2.basePosition)
 			{
-				P1Y = PLATFORM_2_HEIGHT + P1RADIUS;
+				P1Y = platform2.height + P1RADIUS;
 				canJump();
 			}
 		}
 
 		//platform 3
-		if (P1Y - P1RADIUS >= PLATFORM_3_HEIGHT - JUMP_SPEED && P1Y + P1RADIUS <= PLATFORM_3_HEIGHT + P1RADIUS * 2)
+		if (P1Y - P1RADIUS >= platform3.height - JUMP_SPEED && P1Y + P1RADIUS <= platform3.height + P1RADIUS * 2)
 		{
-			if (P1X - P1RADIUS < PLATFORM_3_BASE_POSITION + PLATFORM_3_WIDTH && P1X + P1RADIUS > PLATFORM_3_BASE_POSITION)
+			if (P1X - P1RADIUS < platform3.basePosition + platform3.width && P1X + P1RADIUS > platform3.basePosition)
 			{
-				P1Y = PLATFORM_3_HEIGHT + P1RADIUS;
+				P1Y = platform3.height + P1RADIUS;
 				canJump();
 			}
 		}
 		////////end player 1 stuff
 
-		//if (isItem3Collected == true)
+		//if (item3.isCollected == true)
 		//{
 		//	break;
 		//}
